4de86fb1c404f6f0a158e7645d0f76c7182ac8f5
João Pedro <joao.pedro.a@live.com>
2026-03-07 14:24:27 -0300

    feat: add new exception handler and extend error response model

--- src/main/java/com/joao/AgendaDeAtividades/controller/GlobalExceptionHanble.java
+++ src/main/java/com/joao/AgendaDeAtividades/controller/GlobalExceptionHanble.java
@@ -1,15 +1,18 @@
 package com.joao.AgendaDeAtividades.controller;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.joao.AgendaDeAtividades.exceptions.ActivityNotFoundException;
 import com.joao.AgendaDeAtividades.exceptions.ErrorResponse;
-
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestControllerAdvice
@@ -26,10 +29,23 @@ public class GlobalExceptionHanble {
         status.value(),
         status.getReasonPhrase(),
         ex.getMessage(),
-         request.getRequestURI());
+        request.getRequestURI(),Collections.emptyMap());
 
         return ResponseEntity.status(status).body(error);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidation(MethodArgumentNotValidException ex, HttpServletRequest request){
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        ErrorResponse response = new ErrorResponse(LocalDateTime.now(),status.value(),status.getReasonPhrase(),ex.getMessage(), request.getRequestURI(), errors);
+
+        return ResponseEntity.badRequest().body(response);
+    }
+    
 
 
 
--- src/main/java/com/joao/AgendaDeAtividades/exceptions/ErrorResponse.java
+++ src/main/java/com/joao/AgendaDeAtividades/exceptions/ErrorResponse.java
@@ -1,14 +1,14 @@
 package com.joao.AgendaDeAtividades.exceptions;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+
 
 @Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class ErrorResponse {
@@ -17,6 +17,7 @@ public class ErrorResponse {
     private int status;
     private String error;
     private String message;
-    private String patch;
+    private String path;
+    private Map<String, String> errors;
 
 }
